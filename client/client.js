9d8429fc95c4e456c4588a01450ae356b3039945
nobus <nobus@yandex-team.ru>
2016-01-28 22:46:07 +0300

    Change all functions for pixi.js

--- client/client.js
+++ client/client.js
@@ -1,145 +1,195 @@
 
-function getURLParameter (sParam) {
-  var sPageURL = decodeURIComponent(window.location.search.substring(1));
-  var sURLVariables = sPageURL.split('&');
-  var sParameterName;
+$(document).ready(function() {
+  var globalMap = [];
 
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
+  var playerSprites = {};
 
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined ? true : sParameterName[1];
-    }
+  var stage = new PIXI.Container();
+  var renderer = PIXI.autoDetectRenderer(640, 640);
+
+  var mapContainer = new PIXI.Container();
+
+  mapContainer.x = 0;
+  mapContainer.y = 0;
+
+  stage.addChild(mapContainer);
+
+  $('.canvas-block').append(renderer.view);
+
+  animate();
+
+  var myLogin = getURLParameter('login');
+  var socket = new WebSocket('ws://localhost:8081/');
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(stage);
   }
-}
 
-function addMessageToConsole (message) {
-  var htmlString = $('#console').html() + '<p>' + message + '</p>';
-  $('#console').html(htmlString);
-  var top = $('#console').get(0).scrollHeight;
-  $('#console').scrollTop(top);
-}
+  function getURLParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1));
+    var sURLVariables = sPageURL.split('&');
+    var sParameterName;
 
-function drawSquare (color, y, x) {
-  var elemSize = 7;
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
 
-  $('#labyrinth').drawRect({
-    fillStyle: color,
-    x: x * elemSize,
-    y: y * elemSize,
-    width: elemSize,
-    height: elemSize,
-    fromCenter: false
-  });
-}
-
-function drawMap (labMap) {
-  for (var i = 0; i < labMap.length; i++) {
-    var mapRow = labMap[i];
-
-    for (var ii = 0; ii < mapRow.length; ii++) {
-      var element = mapRow[ii];
-
-      if (element === 1) {
-        var colorStyle = '#000';
-      } else if (element === 0) {
-        var colorStyle = '#fff';
-      }
-
-      drawSquare(colorStyle, i, ii);
-    }
-  }
-}
-
-function changeMap (changeMap) {
-  changeMap.forEach(function (item) {
-    if (item.id === 1) {
-      var colorStyle = '#000';
-    } else if (item.id === 0) {
-      var colorStyle = '#fff';
-    }
-
-    for (var i = 0; i < item.length; i++) {
-      if (item.type === 'vertical') {
-        drawSquare(colorStyle, item.startY + i, item.startX);
-      } else {
-        drawSquare(colorStyle, item.startY, item.startX + i);
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
       }
     }
-  });
-}
-
-function changePosition (changePosition) {
-  var yourColor = '#00aa00';
-  var neighborColor = '#0000aa';
-
-  if (changePosition.login === myLogin) {
-    drawSquare(yourColor, changePosition.y, changePosition.x);
-  } else {
-    drawSquare(neighborColor, changePosition.y, changePosition.x);
   }
 
-  if ('oldY' in changePosition && 'oldX' in changePosition) {
-    drawSquare('#fff', changePosition.oldY, changePosition.oldX);
+  function addMessageToConsole(message) {
+    var htmlString = $('#console').html() + '<p>' + message + '</p>';
+    $('#console').html(htmlString);
+    var top = $('#console').get(0).scrollHeight;
+    $('#console').scrollTop(top);
   }
-}
 
-var myLogin = getURLParameter('login');
-var socket = new WebSocket('ws://localhost:8080/');
+  function drawMap(labMap) {
+    for (var i = 0; i < labMap.length; i++) {
+      globalMap.push([]);
 
-socket.onopen = function () {
-  socket.send(JSON.stringify({'login': myLogin}));
-  addMessageToConsole('Connection done.');
+      var mapRow = labMap[i];
 
-  $(document).keypress(function (event) {
-    if (socket) {
-      var direction;
+      for (var ii = 0; ii < mapRow.length; ii++) {
+      var mapSprite;
+        var element = mapRow[ii];
 
-      if (event.keyCode === 119) {          // w
-        direction = 'up';
-      } else if (event.keyCode === 115) {   // s
-        direction = 'down';
-      } else if (event.keyCode === 97) {    // a
-        direction = 'left';
-      } else if (event.keyCode === 100) {   // d
-        direction = 'right';
-      }
+        if (element === 1) {
+          mapSprite = PIXI.Sprite.fromImage("img/wall.png");
+        } else if (element === 0) {
+          mapSprite = PIXI.Sprite.fromImage("img/ground.png");
+        }
 
-      if (direction) {
-        socket.send(JSON.stringify({'login': myLogin, 'direction': direction}));
+        if (mapSprite) {
+          mapSprite.x = 32 * ii;
+          mapSprite.y = 32 * i;
+
+          mapContainer.addChild(mapSprite);
+
+          globalMap[i].push(mapSprite);
+        }
       }
     }
-
-  });
-};
-
-socket.onclose = function (event) {
-  if (event.wasClean) {
-    addMessageToConsole('Connection closed.');
-  } else {
-    addMessageToConsole('Connection broken.'); // for example, server died
-  }
-  addMessageToConsole('Code: ' + event.code + ' reason: ' + event.reason);
-};
-
-socket.onmessage = function (event) {
-  var rawMessage = event.data;
-  addMessageToConsole('Data received: ' + rawMessage.length);
-  var message = JSON.parse(rawMessage);
-
-  if (message.allMap) {
-    drawMap(message.allMap);
   }
 
-  if (message.changeMap) {
-    changeMap(message.changeMap);
+  function changeMap(changeMap) {
+    changeMap.forEach(function (item) {
+      for (var i = 0; i < item.length; i++) {
+        var mapSprite;
+
+        if (item.id === 1) {
+          mapSprite = PIXI.Sprite.fromImage("img/wall.png");
+        } else if (item.id === 0) {
+          mapSprite = PIXI.Sprite.fromImage("img/ground.png");
+        }
+
+        var y = item.startY;
+        var x = item.startX;
+
+        if (item.type === 'vertical') {
+          y += i;
+        } else {
+          x += i;
+        }
+
+        var oldMapSprite = globalMap[y][x];
+
+        mapSprite.x = oldMapSprite.x;
+        mapSprite.y = oldMapSprite.y;
+
+        mapContainer.removeChild(oldMapSprite);
+        mapContainer.addChild(mapSprite);
+
+        globalMap[y][x] = mapSprite;
+
+      }
+    });
   }
 
-  if (message.changePosition) {
-    changePosition(message.changePosition);
-  }
-};
+  function changePosition(changePosition) {
+    var login = changePosition.login;
 
-socket.onerror = function (error) {
-  addMessageToConsole('Error ' + error.message);
-};
+    if (playerSprites[login] === undefined) {
+      playerSprites[login] = PIXI.Sprite.fromImage('img/player.png');
+
+      if (login === myLogin) {
+        playerSprites[login].y = 320;
+        playerSprites[login].x = 320;
+      }
+
+      stage.addChild(playerSprites[login]);
+    }
+
+    if (login === myLogin) {
+      mapContainer.x = changePosition.x * 32;
+      mapContainer.y = changePosition.y * 32;
+
+    } else {
+      playerSprites[login].y = changePosition.y * 32;
+      playerSprites[login].x = changePosition.x * 32;
+
+    }
+
+  }
+
+  socket.onopen = function () {
+    socket.send(JSON.stringify({'login': myLogin}));
+    addMessageToConsole('Connection done.');
+
+    $(document).keypress(function (event) {
+      if (socket) {
+        var direction;
+
+        if (event.keyCode === 119) {          // w
+          direction = 'up';
+        } else if (event.keyCode === 115) {   // s
+          direction = 'down';
+        } else if (event.keyCode === 97) {    // a
+          direction = 'left';
+        } else if (event.keyCode === 100) {   // d
+          direction = 'right';
+        }
+
+        if (direction) {
+          socket.send(JSON.stringify({'login': myLogin, 'direction': direction}));
+        }
+      }
+
+    });
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      addMessageToConsole('Connection closed.');
+    } else {
+      addMessageToConsole('Connection broken.'); // for example, server died
+    }
+    addMessageToConsole('Code: ' + event.code + ' reason: ' + event.reason);
+  };
+
+  socket.onmessage = function (event) {
+    var rawMessage = event.data;
+    addMessageToConsole('Data received: ' + rawMessage.length);
+    var message = JSON.parse(rawMessage);
+
+    if (message.allMap) {
+      drawMap(message.allMap);
+    }
+
+    if (message.changeMap) {
+      changeMap(message.changeMap);
+    }
+
+    if (message.changePosition) {
+      changePosition(message.changePosition);
+    }
+  };
+
+  socket.onerror = function (error) {
+    addMessageToConsole('Error ' + error.message);
+  };
+
+});
