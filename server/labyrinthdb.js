05131a5eaf5c4bdab431a8de4d86f3ce726fc7e4
nobus <nobus@yandex-team.ru>
2016-03-12 18:15:09 +0300

    Add first prototype of LabyrinthDB.

---
+++ server/labyrinthdb.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var r = require('rethinkdb');
+
+
+export class LabyrinthDB {
+  constructor (conn, dbName) {
+    this.conn = conn;
+    this.dbName = dbName;
+
+    this.tableList = ['chat'];
+
+    this.checkDB();
+  }
+
+  checkDB () {
+    const _this = this;
+
+    r.dbList().run(this.conn, function (err, dbList) {
+      if (err) throw err;
+
+      if (!dbList.find(function (e) {
+          return e === _this.dbName
+        })) _this.createDB();
+    });
+  }
+
+  createDB () {
+    const _this = this;
+
+    r.dbCreate(this.dbName).run(this.conn, function (err, res) {
+      if (err) throw err;
+
+      _this.tableList.forEach(function (tableName) {
+        _this.createTable(tableName);
+      });
+
+    });
+  }
+
+  createTable (tableName) {
+    r.db(this.dbName).tableCreate(tableName).run(this.conn, function (err, res) {
+      if (err) throw err;
+    });
+  }
+}
