5810ebf0221067abb5f245826a81eb513f93306a
nobus <nobus@yandex-team.ru>
2016-04-05 19:25:29 +0300

    Small refactoring

--- server/protodb.js
+++ server/protodb.js
@@ -15,33 +15,39 @@ export class ProtoDB {
   initDB () {
     const _this = this;
 
-    rethinkDB.dbList().run(this.conn, function (err, dbList) {
-      if (err) throw err;
+    rethinkDB
+      .dbList()
+      .run(this.conn, function (err, dbList) {
+        if (err) throw err;
 
-      if (!dbList.find(function (e) {
+        if (!dbList.find(function (e) {
           return e === _this.dbName
         })) _this.createDB();
-      else _this.runDB();
+        else _this.runDB();
     });
   }
 
   createDB () {
     const _this = this;
 
-    rethinkDB.dbCreate(this.dbName).run(this.conn, function (err, res) {
-      if (err) throw err;
+    rethinkDB
+      .dbCreate(this.dbName)
+      .run(this.conn, function (err, res) {
+        if (err) throw err;
 
-      _this.tableList.forEach(function (tableName) {
-        _this.createTable(tableName);
+        _this.tableList.forEach(function (tableName) {
+          _this.createTable(tableName);
+        });
       });
-
-    });
   }
 
   createTable (tableName) {
-    rethinkDB.db(this.dbName).tableCreate(tableName).run(this.conn, function (err, res) {
-      if (err) throw err;
-    });
+    rethinkDB
+      .db(this.dbName)
+      .tableCreate(tableName)
+      .run(this.conn, function (err, res) {
+        if (err) throw err;
+      });
   }
 
   readChanges (tableName, callback) {
