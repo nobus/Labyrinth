67c64ce8dbb504f04bfe7f2ac23fb1efaf3cc114
nobus <nobus@yandex-team.ru>
2016-04-26 00:29:10 +0300

    Add colors and params.

--- server/worldGenerator.js
+++ server/worldGenerator.js
@@ -2,12 +2,19 @@
 
 const rethinkDB = require('rethinkdb');
 const program = require('commander');
+const colors = require('colors');
 const common = require('./common');
 
 var worldDB = 'labyrinth';
 
 
 if (require.main === module) {
+  program
+  .version('0.0.1')
+  .option('-p, --port <n>', 'Port for RethinkDB, default is 28015', parseInt, {isDefault: 28015})
+  .option('-t, --test <n>', 'Create n Corals', parseInt)
+  .parse(process.argv);
+
   rethinkDB.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) throw err;
 
@@ -18,9 +25,9 @@ if (require.main === module) {
 
         for (let i = 0; i < dbList.length; i++) {
           if (dbList[i] === worldDB) {
-            common.log(`The world is exist. `
+            common.log(colors.red(`The world is exist. `
               + `If you really want create a new world, `
-              + `delete the database "${worldDB}".`);
+              + `delete the database "${worldDB}".`));
 
             process.exit(1);
           }
