6d5ab9cfe13a7f6126e8eb15d1b31a33bea75923
nobus <nobus@yandex-team.ru>
2016-04-27 22:42:20 +0300

    Add color log in the world generator

--- server/worldGenerator.js
+++ server/worldGenerator.js
@@ -10,20 +10,21 @@ var worldDB = 'labyrinth';
 if (require.main === module) {
   program
   .version('0.0.1')
+  .option('-n, --dbname', 'Name of world database')
   .option('-p, --port <n>', 'Port for RethinkDB, default is 28015', parseInt, {isDefault: 28015})
   .option('-t, --test <n>', 'Create n Corals', parseInt)
   .parse(process.argv);
 
-  rethinkDB.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+  rethinkDB.connect( {host: 'localhost', port: program.port}, function(err, conn) {
     if (err) throw err;
 
     rethinkDB
-      .dbCreate(worldDB)
+      .dbCreate(program.dbname)
       .run(conn, (err, res) => {
         if (err) {
           log.error(`The world is exist. `
             + `If you really want create a new world, `
-            + `delete the database "${worldDB}".`);
+            + `delete the database "${program.dbname}".`);
 
           throw  err;
         }
