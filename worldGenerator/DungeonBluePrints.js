764a1855a6ce0c91d892f8fea6c0ed3e196ad424
nobus <nobus@yandex-team.ru>
2016-05-12 22:46:10 +0300

    Add blueprints skeleton

---
+++ worldGenerator/DungeonBluePrints.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const common = require('./../server/common');
+
+export class DungeonBluePrints {
+  constructor (number, worldSize) {
+    this.number = number;
+    this.worldSize = worldSize;
+
+    this.blueprints = {};
+  }
+
+
+  generate () {
+
+  }
+}
