7406c251b4815ec49becdb1f17df3e1da8c7e12f
nobus <nobus@yandex-team.ru>
2016-06-08 23:02:49 +0300

    Add methods cacheWorldMap, cacheUserPosition and userPositionChangeFeed

--- server/cartographer.js
+++ server/cartographer.js
@@ -8,15 +8,85 @@ const log = require('./log');
 
 
 class CartographerDB {
-  constructor (conn, dbname, interval) {
+  constructor (conn, dbName, interval) {
     this.conn = conn;
-    this.dbname = dbname;
+    this.dbName = dbName;
     this.interval = interval;
+
+    this.conn.use(this.dbName);
+
+    this.worldMapCache = {};
+    this.userPositionCache = {};
+
+    this.cacheWorldMap();
+    this.cacheUserPosition();
+  }
+
+  cacheWorldMap () {
+    rethinkDB
+      .table('worldMap', {readMode: 'outdated'})
+      .run(this.conn, (err, cursor) => {
+        if (err) throw err;
+
+        cursor.toArray((err, res) => {
+          if (err) throw err;
+
+          for (let i = 0; i < res.length; i++) {
+            let e = res[i];
+            this.worldMapCache[e.location] = e;
+          }
+
+          log.info(`World map cache is ready.`);
+        });
+      });
+  }
+
+  cacheUserPosition () {
+    rethinkDB
+      .table('userPosition', {readMode: 'outdated'})
+      .run(this.conn, (err, cursor) => {
+        if (err) throw  err;
+
+        cursor.toArray( (err, res) => {
+          if (err) throw  err;
+
+          for (let i = 0; i < res.length; i++) {
+            let e = res[i];
+            this.userPositionCache[e.login] = e;
+          }
+
+          log.info(`User position cache is ready.`);
+
+          this.userPositionChangeFeed();
+        });
+      });
+  }
+
+  userPositionChangeFeed () {
+    rethinkDB
+      .table('userPosition', {readMode: 'outdated'})
+      .changes()
+      .run(this.conn, (err, cursor) => {
+        if (err) throw  err;
+
+        cursor.each((err, row) => {
+          if (err) throw err;
+
+          const e = row.new_val;
+          this.userPositionCache[e.login] = e;
+
+          log.info(JSON.stringify(this.userPositionCache));
+        });
+      });
   }
 
   run () {
     setInterval( () => {
-      log.info('Change the Map!');
+      if (common.isEmpty(this.worldMapCache) ||  common.isEmpty(this.userPositionCache)) {
+        log.warn('Not ready caches yet');
+      } else {
+        log.info('Change the Map!');
+      }
     }, this.interval);
   }
 
