21858768f367dba58798723c9edb7831855e66a2
nobus <nobus@yandex-team.ru>
2016-06-17 18:16:24 +0300

    Common config for all

---
+++ config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  'rethink': {
+    'dbhost': 'localhost',
+    'dbport': 28015,
+    'dbname': 'qwerty'
+  },
+  'world': {
+    'dungeons': 3,
+    'wolrdSize': 3,
+    'locationSize': 100
+  },
+  'garden': {
+    'ports': [8081]
+  },
+  'test': {
+    'users': 100,
+    'prefix': 'coral'
+  }
+};
