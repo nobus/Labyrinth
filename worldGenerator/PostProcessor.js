5d0c9573bf9836858c32419ab2970a51cf7ab4c4
Maksim Klimin <meganobus@gmail.com>
2016-08-26 11:12:24 +0300

    Add PostProcessor

---
+++ worldGenerator/PostProcessor.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const log = require('./../server/log');
+
+
+export class PostProcessor {
+  constructor(n) {
+    this.counter;
+
+    if (typeof n === 'number') {
+      this.counter = n;
+    }
+  }
+
+  increment (n) {
+    if (typeof this.counter === 'undefined') {
+      this.counter = 0;
+    }
+
+    this.counter += n;
+  }
+
+  decrement (n) {
+    if (typeof this.counter === 'number') {
+      this.counter -= n;
+
+      if (this.counter <= 0) {
+        this.exit();
+      }
+    }
+  }
+
+  exit () {
+    log.info(`I'm going to exit, bye!`);
+    process.exit();
+  }
+}
