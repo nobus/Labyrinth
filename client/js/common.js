12fc7d348beed581cc79078dac7abfff734d004f
Maksim Klimin <meganobus@gmail.com>
2016-09-06 14:56:18 +0300

    Refactoring. Add file for common functions

---
+++ client/js/common.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function getURLParameter(sParam) {
+  const sPageURL = decodeURIComponent(window.location.search.substring(1));
+  const sURLVariables = sPageURL.split('&');
+  let sParameterName;
+
+  for (let i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+}
