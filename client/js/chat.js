b384630ce7d2fa23d64385d5a564978a89149afa
Maksim Klimin <meganobus@gmail.com>
2016-09-22 16:52:24 +0300

    remove very old legacy

--- client/js/chat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function addMessageToChat(message) {
-  // for the future
-  let htmlString = $('.chat-block').html() + '<p>' + message + '</p>';
-  $('.chat-block').html(htmlString);
-  let top = $('.chat-block').get(0).scrollHeight;
-  $('.chat-block').scrollTop(top);
-}
